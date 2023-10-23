784ed63cbcfc029be9fd66ebb0987b26b9b4ec89
ksarisc <jacobsmith.in@gmail.com>
2023-10-22 21:09:39 -0400

    initial commit

---
+++ ProjectStopwatch/ProjectTimer.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace ProjectStopwatch
+{
+    partial class ProjectTimer
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ProjectLabel = new System.Windows.Forms.Label();
+            this.TimerBox = new System.Windows.Forms.TextBox();
+            this.TimerButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ProjectLabel
+            // 
+            this.ProjectLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ProjectLabel.AutoSize = true;
+            this.ProjectLabel.Location = new System.Drawing.Point(14, 11);
+            this.ProjectLabel.Margin = new System.Windows.Forms.Padding(5);
+            this.ProjectLabel.Name = "ProjectLabel";
+            this.ProjectLabel.Size = new System.Drawing.Size(71, 13);
+            this.ProjectLabel.TabIndex = 0;
+            this.ProjectLabel.Text = "Project Name";
+            // 
+            // TimerBox
+            // 
+            this.TimerBox.Location = new System.Drawing.Point(167, 8);
+            this.TimerBox.Name = "TimerBox";
+            this.TimerBox.Size = new System.Drawing.Size(100, 20);
+            this.TimerBox.TabIndex = 1;
+            // 
+            // TimerButton
+            // 
+            this.TimerButton.Location = new System.Drawing.Point(343, 6);
+            this.TimerButton.Name = "TimerButton";
+            this.TimerButton.Size = new System.Drawing.Size(75, 23);
+            this.TimerButton.TabIndex = 2;
+            this.TimerButton.Text = "Start";
+            this.TimerButton.UseVisualStyleBackColor = true;
+            this.TimerButton.Click += new System.EventHandler(this.TimerButton_Click);
+            // 
+            // ProjectTimer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.TimerButton);
+            this.Controls.Add(this.TimerBox);
+            this.Controls.Add(this.ProjectLabel);
+            this.Name = "ProjectTimer";
+            this.Size = new System.Drawing.Size(730, 177);
+            this.Load += new System.EventHandler(this.ProjectTimer_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label ProjectLabel;
+        private System.Windows.Forms.TextBox TimerBox;
+        private System.Windows.Forms.Button TimerButton;
+    }
+}
---
+++ ProjectStopwatch/ProjectTimer.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ProjectStopwatch
+{
+    public partial class ProjectTimer : UserControl
+    {
+        private readonly System.Threading.Timer timer;
+        private readonly Action<string> updateTime;
+
+        private bool running = false;
+        private DateTime? start;
+
+        public ProjectTimer()
+        {
+            InitializeComponent();
+            timer = new System.Threading.Timer(Callback, null, System.Threading.Timeout.Infinite, System.Threading.Timeout.Infinite);
+            updateTime = new Action<string>(UpdateTime);
+        }
+
+        private void ProjectTimer_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Callback(object state)
+        {
+            if (!running)
+            {
+                timer.Change(System.Threading.Timeout.Infinite, System.Threading.Timeout.Infinite);
+                return;
+            }
+
+            string value;
+            if (start == null)
+            {
+                start = DateTime.Now;
+                value = "00:00:00";
+            }
+            else
+                value = DateTime.Now.Subtract(start.Value).ToString(@"hh\:mm\:ss");
+            TimerBox.Invoke(updateTime, new object[] { value });
+        }
+
+        private void UpdateTime(string value)
+        {
+            TimerBox.Text = value;
+        }
+
+        private void TimerButton_Click(object sender, EventArgs e)
+        {
+            running = true;
+            timer.Change(1000, 1000);
+        }
+    }
+}
